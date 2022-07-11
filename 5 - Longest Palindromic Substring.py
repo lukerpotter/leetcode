3156e247e37b5a640038f51c0a453a59f5c4645d
lukerpotter <potterlu@sasktel.net>
2022-07-11 17:46:57 -0600

    Problem 5 - attempt 2 - recursive

--- 5 - Longest Palindromic Substring.py
+++ 5 - Longest Palindromic Substring.py
@@ -1,32 +1,65 @@
 '''
 https://leetcode.com/problems/longest-palindromic-substring/
 
-Initial Attempt: 2022-07-10
+Given a string s, return the longest palindromic substring in s. Constraints:
+1 <= s.length <= 1000
+s consist of only digits and English letters.
 
-First approach: start with the full string. If it is not a palindrome, check
-all strings that are one position shorter on the next iteration. Eventually,
-you will either find a palindrome or return any letter in the string (a "one-
-character palindrome")
+Second Attempt: 2022-07-11
+
+Outer loop: iterates over the string as passed in. Finds strings of consecutive
+letters, then passes this string, the index of the letter prior to it, and
+the index of the letter after it, to the recursive function
+
+Recursive function: using the indices passed in, checks if these two values
+match. If so, add to each end of the string, then decrement the left index
+and increment the right index, and call the function again.
 '''
 
-
 class Solution:
+
     def longestPalindrome(self, s: str) -> str:
-        # What I'm trying to do here:
-        # Pass 1: get all strings of length x
-        # Pass 2: get all strings of length x -1
-        # ...
 
-        for str_length in range(len(s), 0, -1):
+        max_string = ""
 
-            num_possible_solutions_of_length = len(s) - str_length + 1
+        def recurse_thing(palindromic_string: str, left_idx: int,
+                          right_idx: int):
 
-            for idx in range(0, num_possible_solutions_of_length):
-                string_to_check = s[idx:idx+str_length]
-                rev_string = string_to_check[::-1]
-                if string_to_check == rev_string:
-                    return string_to_check
+            if left_idx >= 0 and right_idx < len(s):
+
+                if s[left_idx] == s[right_idx]:
+                    palindromic_string = s[left_idx] + palindromic_string + s[
+                        right_idx]
+
+                    palindromic_string = recurse_thing(palindromic_string,
+                                                       left_idx - 1,
+                                                       right_idx + 1)
+
+            return palindromic_string
+
+        #######################################################################
+        running_idx = 0
+        while running_idx < len(s):
+
+            starting_idx = running_idx
+            running_idx += 1
+
+            palindrome_string = s[starting_idx]
+            while running_idx < len(s) and \
+                    s[starting_idx] == s[running_idx]:
+                palindrome_string += s[running_idx]
+                running_idx += 1
+                #print(palindrome_string)
+
+            word = recurse_thing(palindrome_string, left_idx=starting_idx - 1,
+                                 right_idx=running_idx)
+
+            if len(word) > len(max_string):
+                max_string = word
+
+        return max_string
 
 test = Solution()
-print(test.longestPalindrome(s = "abccbd"))
-
+print(test.longestPalindrome(s="babad"))
+print(test.longestPalindrome(s = "abcccccbd"))
+print(test.longestPalindrome(s = "cbaaaaaaaabcd"))
